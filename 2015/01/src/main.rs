fe2afe7fa08f1aba759e99a7a084228abc53fef7
Daniele Perot <daniele.perot@nordhealth.com>
2024-12-14 15:14:22 +0200

    Solve day two

--- 2015/01/src/main.rs
+++ 2015/01/src/main.rs
@@ -5,7 +5,7 @@ mod solution;
 
 fn main() -> Result<(), Error> {
     let input = fs::read_to_string("input.txt")?;
-    println!("The answer is {}", solution::solve(&input));
+    println!("The answer is {}", solution::solve(&input, solution::StopAt::Floor(-1)));
 
     Ok(())
 }
--- 2015/01/src/solution.rs
+++ 2015/01/src/solution.rs
@@ -1,10 +1,24 @@
-pub fn solve(input: &str) -> i32 {
+pub enum StopAt {
+    None,
+    Floor(i32)
+}
+
+pub fn solve(input: &str, stop_at: StopAt) -> i32 {
     let mut accumulator = 0;
-    for character in input.chars() {
+    for (index, character) in input.chars().into_iter().enumerate() {
         match character {
             '(' => accumulator += 1,
             ')' => accumulator -= 1,
             _ => {}
+        }
+
+        match stop_at {
+            StopAt::None => {}
+            StopAt::Floor(floor) => {
+                if accumulator == floor {
+                    return (index + 1) as i32;
+                }
+            }
         }
     }
 
@@ -13,32 +27,38 @@ pub fn solve(input: &str) -> i32 {
 
 #[test]
 fn empty_data_returns_0() {
-    assert_eq!(0, solve(""));
+    assert_eq!(0, solve("", StopAt::None));
 }
 
 #[test]
 fn open_parens_returns_1() {
-    assert_eq!(1, solve("("));
+    assert_eq!(1, solve("(", StopAt::None));
 }
 
 #[test]
 fn close_parens_returns_neg_1() {
-    assert_eq!(-1, solve(")"));
+    assert_eq!(-1, solve(")", StopAt::None));
 }
 
 #[test]
 fn multiple_open_parens_return_sum_of_open_parens() {
-    assert_eq!(3, solve("((("));
+    assert_eq!(3, solve("(((", StopAt::None));
 }
 
 #[test]
 fn multiple_close_parens_return_neg_sum_of_close_parens() {
-    assert_eq!(-3, solve(")))"));
+    assert_eq!(-3, solve(")))", StopAt::None));
 }
 
 #[test]
 fn other_characters_are_ignored() {
-    assert_eq!(-3, solve(")ahd3h  )3y5gef32@!)"));
+    assert_eq!(-3, solve(")ahd3h  )3y5gef32@!)", StopAt::None));
+}
+
+#[test]
+fn returns_position_of_first_time_a_floor_is_reached_if_floor_is_specified() {
+    assert_eq!(4, solve("((((", StopAt::Floor(4)));
+    assert_eq!(6, solve("((()(()()()", StopAt::Floor(4)));
 }
 
 #[test]
@@ -61,6 +81,6 @@ fn verify_examples() {
     ];
 
     for (index, (input, expected)) in assertions.into_iter().enumerate() {
-        assert_eq!(expected, solve(input), "[#{}] {} should return {}", index, input, expected);
+        assert_eq!(expected, solve(input, StopAt::None), "[#{}] {} should return {}", index, input, expected);
     }
 }
