80fe270104b4d89613e2bacfd9ea38d08ea7e4b0
Daniele Perot <daniele.perot@nordhealth.com>
2024-12-16 21:26:30 +0200

    Complete part 1

--- 2024/01/src/main.rs
+++ 2024/01/src/main.rs
@@ -1,5 +1,11 @@
+use std::fs;
+use std::io::Error;
+
 mod solution;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Error> {
+    let input = fs::read_to_string("input.txt")?;
+    println!("Answer is: {}", solution::distance_from_string(&input));
+
+    Ok(())
 }
